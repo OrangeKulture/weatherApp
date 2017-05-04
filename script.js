fe9b227a70f243ade6c7055bec7e59b5f5296054
Jorge A <ardonz@gmail.com>
2017-05-04 02:05:55 -0600

    mobile friendly updates

--- script.js
+++ script.js
@@ -8,96 +8,104 @@ $(document).ready(function() {
       lat = position.coords.latitude;
       long = position.coords.longitude;
 
-      //API call
+      var cityInfo =
+        "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+        lat +
+        "," +
+        long +
+        "&sensor=true";
 
-      var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric&APPID=10806f1d4ce199266fd576ecf86406da';
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          var resp = JSON.parse(xhttp.responseText);
-          var temp = Math.floor(resp.main.temp);
-          outDegree = temp;
-          var weather = resp.weather[0].id;
-          var name = resp.name;
-          var hum = resp.main.humidity;
-          var wind = resp.wind.speed;
-          var main = resp.weather[0].main;
-          var desc = resp.weather[0].description;
+      $.getJSON(cityInfo, function(data) {
+        var address = data.results[0].address_components[1].long_name;
+        $("#cityName").text(address);
+      });
 
-          //Dynamic Weather Condition Data
+      var url =
+        "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/8bfe73adbee0a652190e6c9ccd1e1d21/" +
+        lat +
+        "," +
+        long +
+        "?exclude=minutely,hourly,daily,alerts,flags&units=si";
 
-          var snow = 'https://s25.postimg.org/gtlrm0qv3/snow.jpg';
-          var clear = 'https://s25.postimg.org/7s2muo3jz/clear.jpg';
-          var rain = 'https://s25.postimg.org/w4vkmmo73/rain.png';
-          var clouds = 'https://s25.postimg.org/fyz7zzj0f/clouds.jpg';
-          var thunder = 'https://s25.postimg.org/kehn58ven/thunder.jpg';
-          var drizzle = 'https://s25.postimg.org/faqdh1kan/drizzle.jpg';
-          var extreme = 'https://s25.postimg.org/8yb877h8f/extreme.jpg';
-          var atmosphere = 'https://s25.postimg.org/xz3tqmltr/atmosphere.jpg';
-          var img = '';
+      $.ajax({
+        url: url,
+        type: "GET",
+        async: false,
+        dataType: "json",
+        success: function(data, status) {
+          var weather = data.currently.icon;
+          var description = data.currently.summary;
+          outDegree = data.currently.temperature;
+          var hum = data.currently.humidity * 100;
+          var winds = data.currently.windSpeed;
 
-          
-                if (weather === 800) {
-                            img = clear;
-                    }else if(weather<299){
-                            img = thunder;    
-                   }else if(weather>299&&weather<350){
-                            img = drizzle;
-                   }else if(weather>499&weather<540){
-                            img = rain;
-                   }else if(weather>500&&weather<625){
-                            img = snow;
-                   }else if(weather>700&&weather<785){
-                            img = atmosphere;
-                    }else if(weather>800&&weather<805){
-                            img = clouds;
-                    }else if(weather<899&&weather<907){
-                            img = extreme;
-                    };
-          
-                                              
-          $('body').css('backgroundImage', 'url(' + img + ')');
+          var snow = "https://s25.postimg.org/gtlrm0qv3/snow.jpg";
+          var clearDay = "https://s25.postimg.org/7s2muo3jz/clear.jpg";
+          var clearNight = "https://s25.postimg.org/y35hzs3r3/sky-moon.jpg";
+          var rain = "https://s25.postimg.org/w4vkmmo73/rain.png";
+          var cloudyDay = "https://s25.postimg.org/fyz7zzj0f/clouds.jpg";
+          var cloudyNight = "https://s25.postimg.org/sqgnlnfun/cloudy-night.jpg";
+          var sleet = "https://s25.postimg.org/x259anmrj/sleet.jpg";
+          var wind = "https://s25.postimg.org/8yb877h8f/extreme.jpg";
+          var foggy = "https://s25.postimg.org/xz3tqmltr/atmosphere.jpg";
+          var img = "";
 
-          $('#cityName').text(name);
-          $('#main').text(main);
-          $('#desc').text(desc);
-          $('#degree').text(temp + '°');
-          $('#humid').text(hum);
-          $('#knot').text(wind);
+          if (weather === "clear-day") {
+            img = clearDay;
+          } else if (weather === "clear-night") {
+            img = clearNight;
+          } else if (weather === "rain") {
+            img = rain;
+          } else if (weather === "snow") {
+            img = snow;
+          } else if (weather === "sleet") {
+            img = sleet;
+          } else if (weather === "wind") {
+            img = wind;
+          } else if (weather === "fog") {
+            img = foggy;
+          } else if (weather === "cloudy" || weather === "partly-cloudy-day") {
+            img = cloudyDay;
+          } else if (weather === "partly-cloudy-night") {
+            img = cloudyNight;
+          }
 
+          $("body").css("backgroundImage", "url(" + img + ")");
+          $("#desc").text(description);
+          $("#degree").text(Math.floor(outDegree) + "°");
+          $("#humid").text(hum + "%");
+          $("#knot").text(winds);
         }
-      };
-      xhttp.open("GET", url, true);
-      xhttp.send();
-
+      });
     });
-
   }
-  
-
-  // Animations and CSS 
+  // Animations and CSS
   setInterval(function() {
-    $('.headerText,.stats').addClass('animated pulse')
+    $(".title h1,.stats").addClass("animated pulse");
   }, 1000);
 
   setInterval(function() {
-    $('#lett1,#lett2,#lett3').css('color', '#E04006');
+    $("#lett1,#lett2,#lett3").css("color", "#E04006");
   }, 1080);
 
   // Metric Units Change
-  var rightTemp = document.getElementById('degree');
+  var rightTemp = document.getElementById("degree");
 
-  var animation = 'animated shake';
-  $('.temp').on('click', function() {
-    $('.icon').addClass(animation).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
-      $(this).removeClass(animation);
-    });
-    $('#degree').text(outDegree + '°');
+  var animation = "animated shake";
+  $(".temp").on("click", function() {
+    $(".icon")
+      .addClass(animation)
+      .one(
+        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",
+        function() {
+          $(this).removeClass(animation);
+        }
+      );
+    $("#degree").text(Math.floor(outDegree) + "°");
   });
 
-  $('.temp2').on('click', function() {
+  $(".temp2").on("click", function() {
     var faren = Math.floor(outDegree * 9 / 5 + 32);
-    $('#degree').text(faren + '°');
+    $("#degree").text(faren + "°");
   });
-
 });
